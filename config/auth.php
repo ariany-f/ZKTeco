a9b22aa2f78a9ad8270063f8ebdd14f7047b9488
ariany-f <ariany_f@hotmail.com>
2023-12-16 00:49:20 -0300

    first commit

---
+++ config/auth.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'facebook' => [
+        'client_id'         => '',
+        'client_secret'     => '',
+        'graph_api_version' => 'v12.0'
+    ],
+    'google' => [
+        'client_id'         => '',
+        'client_secret'     => ''
+    ]
+];
---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'driver' => 'mysql',
+	'mysql' => [
+		'host' 		=> 'localhost',
+		'username' 	=> 'root',
+		'password' 	=> 'Mudar123',
+		'database' 	=> 'lojavirtual',
+		'port' 		=> '80',
+		'charset' 	=> 'utf8mb4',
+		'collation' => 'utf8mb4_unicode_ci'
+	]
+];
