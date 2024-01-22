5a255bcdf128e8cdc429c14d3a0693e206e92bcd
ariany-f <ariany_f@hotmail.com>
2024-01-22 14:28:03 -0300

    pendente

--- app/Controllers/Panel/PendingController.php
+++ app/Controllers/Panel/PendingController.php
@@ -39,6 +39,8 @@ class PendingController extends Controller{
 
         $data['pending'] = 0;
 		if($client->update($data)){
+            echo '<pre>';
+            print_r($client);die;
 			redirect(route('panel.pending'), ['success' => 'Cliente aprovado com sucesso']);
 		}
 
